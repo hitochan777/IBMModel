0c52e5bb5831a4986ebb686ae6597224d505f649
Hitoshi Otsuki <ilikestars55@yahoo.co.jp>
2014-06-23 02:50:36 +0900

    rubbish

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,7 +6,7 @@ int main(int argc,char** argv){
 	//IBM1 ibm1("toy.de","toy.en");
 	//IBM1 ibm1("toy2.ge","toy2.en");
 	//IBM1 ibm1("fr-en.tiny.fr","fr-en.tiny.fr");
-	IBM2 ibm("fr-en.tiny.fr","fr-en.tiny.en");
+	IBM1 ibm("fr-en.tiny.fr","fr-en.tiny.en");
 	ibm.writeTranslationTableToFile(output);
 	return 0;
 }
